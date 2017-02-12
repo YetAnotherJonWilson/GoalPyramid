d60e52ad975bd78551e9bb856a4719513472ecb0
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-02-12 04:26:38 -0600

    adding actions and state variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import C from './constants'
+import { goals, dailyTasks }from './initialState.json'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+console.log(`
+
+             Goals
+  =============================
+  The initial goal is "${goals}"
+  There are ${dailyTasks.length} tasks in state
+  
+  Constants (actions)
+  -------------------
+  ${Object.keys(C).join('\n  ')}
+  
+`)
